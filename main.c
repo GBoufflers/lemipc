46263b094f5dcbc2403af3fd7c8830be0f75d5ab
guillaume <guillaume@debian.local.localhost>
2013-03-26 16:24:14 -0400

    ok

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ void		unlock(struct sembuf *sops)
 
 int		get_rand_number()
 {
-  return (rand() % 50);
+  return (rand() % 25);
 }
 
 int		count_team(char *map, char c)
@@ -40,6 +40,16 @@ int		is_quitting(char *addr, char team, int pos)
   return (0);
 }
 
+void		first_player()
+{
+
+}
+
+void		others()
+{
+
+}
+
 int		main(int ac, char **av)
 {
   key_t		key;
@@ -55,15 +65,15 @@ int		main(int ac, char **av)
     }
   srand(time(NULL));
   key = ftok(av[1], 0);
-  printf("Key : %d\n", key);
   shm_id = shmget(key, 42, SHM_R | SHM_W);
   printf("shmId = %d\n", shm_id);
   if (shm_id == -1)
     {
+      //      first_player();
       sem_id = semget(key, 1, SHM_R | SHM_W | IPC_CREAT);
       shm_id = shmget(key,  42, IPC_CREAT | SHM_R | SHM_W);
       addr = shmat(shm_id, NULL, SHM_R | SHM_W);
-      sprintf((char *)addr, "ooooooooooooooooooooooooooooooooooooooooooooo");
+      sprintf((char *)addr, "ooooooooooooooooooooooooo");
       printf("Created shm segment %d\n", shm_id);
       semctl(sem_id, 0, SETVAL, 1);
       while (1)
