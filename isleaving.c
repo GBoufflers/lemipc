7293b06a7aacad9f498bc2f391854929a89e9977
guillaume <guillaume@debian.local.localhost>
2013-03-27 16:56:11 -0400

    ok

--- isleaving.c
+++ isleaving.c
@@ -1,19 +1,13 @@
 #include	"lemipc.h"
 
+
+
 int		is_quitting(char *addr, char team, int pos)
 {
-  char		**map;
-  int		i;
-  int		j;
-  int		k;
-
-  i = 0;
-  k = 0;
-  map = malloc(6 * sizeof(char *));
-  while (addr[i] != '\0')
-    {
-
-      i++;
-    }
+  int		x;
+  int		y;
+  
+  x = getX();
+  y = getY();
   return (0);
 }
