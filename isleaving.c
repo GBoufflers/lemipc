5585cd26a08617af0626c22e214dbdbe7aaaeee0
guillaume <guillaume@debian.local.localhost>
2013-03-27 16:58:21 -0400

    ok

--- isleaving.c
+++ isleaving.c
@@ -1,13 +1,21 @@
 #include	"lemipc.h"
 
+int		getX(int pos)
+{
 
+}
+
+int		getY(int pos)
+{
+
+}
 
 int		is_quitting(char *addr, char team, int pos)
 {
   int		x;
   int		y;
   
-  x = getX();
-  y = getY();
+  x = getX(pos);
+  y = getY(pos);
   return (0);
 }
